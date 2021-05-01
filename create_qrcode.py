54b81f3b66e3e1a840fd77114407b405749d9cc4
vutriancode <vutrian576@gmail.com>
2021-05-01 15:17:42 +0700

    update

--- create_qrcode.py
+++ create_qrcode.py
@@ -63,7 +63,7 @@ async def encode_qr(request: Request):
         "resident_addr" : body['actor_represent_resident_addr']
     }
 
-    context_to_load['qr_code'] = InlineImage(doc, "vutrian.png",width=Mm(35gi))
+    context_to_load['qr_code'] = InlineImage(doc, "vutrian.png",width=Mm(35))
 
 
     # load the context to the word template
