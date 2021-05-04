4039253bbde50ba9f858d97033e2f995d8338f68
vutriancode <vutrian576@gmail.com>
2021-05-04 13:16:00 +0700

    update

--- decode.py
+++ decode.py
@@ -1,7 +1,13 @@
-import cv2
-image = cv2.imread('mn.JPG')
-cv2.imshow("a", image)
-cv2.waitKey()
-qrCodeDetector = cv2.QRCodeDetector()
-decodedText, points, _ = qrCodeDetector.detectAndDecode(image)
-print(decodedText)
+# import cv2
+# from pdf2image import convert_from_path
+# pages =convert_from_path("CamScanner.pdf")
+# pages[0].save('out.jpg', 'JPEG')
+# image = cv2.imread("MM.JPG")
+# cv2.imshow("a",image)
+# cv2.waitKey()
+# qrCodeDetector = cv2.QRCodeDetector()
+# decodedText, points, _ = qrCodeDetector.detectAndDecode(image)
+# print(decodedText)
+import qrtools
+qr = qrtools.QR()
+qr.decode("horn.png")
